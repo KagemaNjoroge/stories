d1a6487508cde9b1b75a0596f5620b92224a0389
Kagema Njoroge <reecejames934@gmail.com>
2025-03-09 00:29:32 +0300

    Add Facebook icon to socials component and remove RSS link

--- src/lib/ui/header/socials.svelte
+++ src/lib/ui/header/socials.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import { RSS, X } from '$lib/icons'
+	import { Facebook, X } from '$lib/icons'
 	import * as config from '$lib/site/config'
 </script>
 
 <div class="socials">
+	<a href={config.facebook} target="_blank" rel="noreferrer">
+		<Facebook width={24} height={24} aria-label="Facebook" />
+	</a>
 	<a href={config.twitter} target="_blank" rel="noreferrer">
 		<X width={24} height={24} aria-label="Twitter" />
-	</a>
-	<a href="/rss.xml" target="_blank">
-		<RSS width={24} height={24} aria-label="RSS feed" />
 	</a>
 </div>
 
